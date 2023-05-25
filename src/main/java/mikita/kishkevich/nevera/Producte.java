3e4e67cb86ee47aa7dfcd9c74e04adf367c4be92
Mikita <nikita.kishkevich@gmail.com>
2023-05-25 16:36:39 +0200

    Convertir CSV A Binari acabat

--- src/main/java/mikita/kishkevich/nevera/Producte.java
+++ src/main/java/mikita/kishkevich/nevera/Producte.java
@@ -3,27 +3,34 @@ package mikita.kishkevich.nevera;
 import mikita.kishkevich.Fitxers;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Producte {
     //<editor-fold desc="Propietats">
+    private int id;
     private String nom;
     private Double preu;
     private String desc;
-    private String dataCaducitat;
+    private LocalDate dataCaducitat;
     //</editor-fold>
 
     //<editor-fold desc="Fitxers">
-    private static final String fitxer = ".data/productes.dat";
-    private static final Fitxers file = new Fitxers(fitxer);
+    private static final String fitxerCSV = ".data/productes.csv";
+    private static final String fitxerDAT = ".data/productes.dat";
+    private static final Fitxers file = new Fitxers(fitxerDAT);
     //</editor-fold>
 
     //<editor-fold desc="Constructors">
     public Producte() {
     }
 
-    public Producte(String nom, Double preu, String desc, String dataCaducitat) {
+    public Producte(String nom, Double preu, String desc, LocalDate dataCaducitat) {
+        // id unic per objecte
+        this.id = System.identityHashCode(this);
+        // resta de parametres
         this.nom = nom;
         this.preu = preu;
         this.desc = desc;
@@ -44,7 +51,7 @@ public class Producte {
         return desc;
     }
 
-    public String getDataCaducitat() {
+    public LocalDate getDataCaducitat() {
         return dataCaducitat;
     }
     //</editor-fold>
@@ -65,7 +72,20 @@ public class Producte {
         List<Object> objs = file.retornaFitxerObjecteEnLlista();
         return converteixObjProducte(objs);
     }
-    public void convertirCSVaBinari(){
+
+    public void convertirCSVaBinari() throws IOException {
+        String text = file.retornaFitxerTextNIO();
+        String []linies = text.split("\n");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        for (int i = 0; i < linies.length; i++) {
+            String []param = linies[i].split(";");
+            String nom = param[0];
+            Double preu = Double.parseDouble(param[1]);
+            String desc = param[2];
+            LocalDate dataCaducitat = LocalDate.parse(param[3],formatter);
+            Producte prod = new Producte(nom, preu, desc, dataCaducitat);
+            file.escriuObjecteFitxer(prod, true);
+        }
 
     }
 
